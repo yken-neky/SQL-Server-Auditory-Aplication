b26d65300d7424ee45d30cddb615a5645f97775c
Yan Ken <yanluisgp@gmail.com>
2024-12-09 20:27:09 -0500

    Estado Inicial del proyecto || Version - 0.0

---
+++ App/Backend/Connecting_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ConnectingAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Connecting_App'
---
+++ App/Backend/LandingP_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LandingpAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LandingP_App'
