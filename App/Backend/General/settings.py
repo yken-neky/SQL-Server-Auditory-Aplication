57da1eaa2ff884dbf7e035697bd4210de68959ad
Yan Ken <yanluisgp@gmail.com>
2024-12-09 20:37:38 -0500

    Corregidos error de nuevo nombre del proyecto. (Backend -> General) || Version - 0.0.1

--- App/Backend/General/settings.py
+++ App/Backend/General/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Backend.urls'
+ROOT_URLCONF = 'General.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Backend.wsgi.application'
+WSGI_APPLICATION = 'General.wsgi.application'
 
 
 # Database
